47a547f528d05b191dba63bf66c2e299f71d5012
HIST\dham <dham@hist.co.kr>
2021-08-11 10:00:20 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer'
+
+const customers = [
+  {
+    'id': 1,
+    'image': 'http://placeimg.com/64/64/1',
+    'name': '함동균',
+    'birthday': '770301',
+    'gender': '남자',
+    'job': '천재'
+  },
+  {
+    'id': 2,
+    'image': 'http://placeimg.com/64/64/2',
+    'name': '홍길동',
+    'birthday': '781224',
+    'gender': '남자',
+    'job': '황제'
+  },
+  {
+    'id': 3,
+    'image': 'http://placeimg.com/64/64/3',
+    'name': '평산이',
+    'birthday': '891211',
+    'gender': '남자',
+    'job': '개발자'
+  }
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system.</h2>
+    <div>
+      {
+        customers.map(c => {
+          return(
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          )
+        })
+      }
     </div>
+    
   );
 }
 
